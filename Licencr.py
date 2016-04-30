b0f7d4c2781a9ee57b0d2f7ee1714b869574cf49
Solomon Bičakćić <solomon@ghostmail.com>
2016-05-01 00:12:54 +0200

    Update Licencr.py

--- Licencr.py
+++ Licencr.py
@@ -1,16 +1,11 @@
-import urllib,tempfile,shutil
-def checkFor(app):
-	tmpdir = tempfile.mkdtemp()
-	saveto = tmpdir + '\FL'
-	wheretocheck = "https://raw.githubusercontent.com/SolomonBicakcic/Licencr/master/" + str(app)
+import requests
+def checkLicense(app):
+	verify = "https://raw.githubusercontent.com/SolomonBicakcic/Licencr/master/" + str(app)
 	try:
-		urllib.urlretrieve(wheretocheck, saveto)
-	except:
-		return False
-		
-	for line in open(saveto, "r"):
-		if line[0] == str(1):
+		r = requests.get(verify)
+		if r.status_code is 200:
 			return True
 		else:
 			return False
-	shutil.rmtree(tmpdir)
+	except:
+		return False
